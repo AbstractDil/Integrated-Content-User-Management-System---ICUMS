88dae675f7b31defdf0565d30a7360d3824b04b2
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2023-02-14 14:49:11 +0530

    Update _dbconnect.php

--- Partials/_dbconnect.php
+++ Partials/_dbconnect.php
@@ -4,13 +4,7 @@ $username = "root";
 $password = "";
 $db = "combined";
 
-/* 
-$servername = "localhost";
-$username = "nandysag_abstract";
-$password = "VyuP!(fX[WS8";
-$db = "nandysag_feedback";
 
-*/
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password , $db);
