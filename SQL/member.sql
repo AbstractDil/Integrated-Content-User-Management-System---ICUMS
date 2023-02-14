1449995e7b77330f03ef8499b495d501323772ce
Sagar Nandy <82097180+AbstractDil@users.noreply.github.com>
2023-02-14 14:49:55 +0530

    Update member.sql

--- SQL/member.sql
+++ SQL/member.sql
@@ -43,11 +43,6 @@ CREATE TABLE `member` (
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`id`, `Date_Time`, `name`, `email`, `password`, `uid`, `ip`, `role`, `activity`) VALUES
-(8, '2022-09-28 22:32:22 PM', 'Sagar', 'sagar2016nandy@gmail.com', '$2y$10$BTVm8Xfa24e/Me5XyZDBYeKTW6oW9ZoRWGywwI0iJi/zs8VkJv8LG', 'MCU202294269', '::1', 1, 1),
-(9, '2022-09-28 22:36:14 PM', 'Sagar Nandy', 'nandysagar@yahoo.com', '$2y$10$gVVvHaf9jNOurBIl4RbJmeto3hL7e3PCesp0TD9EvAJ0FLznxPRve', 'MCU202252469', '::1', 2, 0),
-(10, '2022-09-28 22:51:35 PM', 'Sagar555', 'mathhub.sagar@gmail.com', '$2y$10$gflBLXeGa25xgFtjoArPsOwM1T54fNS.VYG6l5VJE8/wiSiNz7KwG', 'MCU202283517', '::1', 0, 0),
-(12, '2022-09-30 13:00:15 PM', 'Dipangshu DP', 'm03710576@gmail.com', '$2y$10$lTaa.Hc/3VQvGAoq6LfIvO5iP82uKgC4U9Izoy2qCC4Vtq7naaSyO', 'MCU202223758', '192.168.0.103', 0, 0);
 
 --
 -- Indexes for dumped tables
